5c48ca25bd47f9517230d07220482f79423a91db
Daniel Parks <dp+git@oxidized.org>
2019-06-19 02:38:07 -0700

    Use puppet strings to generate documentation.

--- README.md
+++ README.md
@@ -26,3 +26,12 @@ archive in that directory.
 ## Limitations
 
 This does not support Windows.
+
+## Reference
+
+There is specific documentation for individual parameters in
+[REFERENCE.md](REFERENCE.md). That file is generated with:
+
+~~~
+pdk bundle exec puppet strings generate --format markdown
+~~~
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,31 @@
-# Install go in /usr/local/go and /usr/local/bin
+# @summary Install go in /usr/local/go and /usr/local/bin
+#
+# This installs go under `/usr/local/go/`, and symlinks the binaries into
+# `/usr/local/bin/`. It also downloads the archive into
+# `/usr/local/src/puppet-golang.tar.gz`.
 #
 # Most people will not need to change any parameter other than `$version`.
+#
+# @param version
+#   The version of Go to install. You can find the latest version number at
+#   https://golang.org/dl/
+# @param link_binaries
+#   The binaries to symlink into `/usr/local/bin`.
+# @param archive_path
+#   Where to store the downloaded archive.
+# @param source
+#   URL to actual archive.
+# @param source_prefix
+#   URL to directory that contains the archive to download.
+# @param os
+#   The OS to use to determine what archive to download.
+# @param arch
+#   The architecture to use to determine what archive to download.
 class golang (
   String[1]        $version       = '1.11',
   Array[String[1]] $link_binaries = ['go', 'gofmt', 'godoc'],
   String[1]        $archive_path  = '/usr/local/src/puppet-golang.tar.gz',
   String[1]        $source_prefix = 'https://dl.google.com/go',
-  String[1]        $source        = "${source_prefix}/go${version}.${os}-${arch}.tar.gz",
   String[1]        $os            = $facts['kernel'] ? {
     'Linux'  => 'linux',
     'Darwin' => 'darwin',
@@ -18,6 +37,7 @@ class golang (
     'i686'   => '386',
     default  => $facts['os']['hardware'],
   },
+  String[1]        $source        = "${source_prefix}/go${version}.${os}-${arch}.tar.gz",
 ) {
   $archive_directory = regsubst($archive_path, '/[^/]+\Z', '')
   if ! defined(File[$archive_directory]) {
