09881626faf808bbc0007068da242642a3e674ff
Daniel Parks <dp+git@oxidized.org>
2019-06-19 02:19:10 -0700

    Align hashrockets in parameters.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,22 +2,22 @@
 #
 # Most people will not need to change any parameter other than `$version`.
 class golang (
-  String[1] $version = '1.11',
+  String[1]        $version       = '1.11',
   Array[String[1]] $link_binaries = ['go', 'gofmt', 'godoc'],
-  String[1] $archive_path = '/usr/local/src/puppet-golang.tar.gz',
-  String[1] $os = $facts['kernel'] ? {
+  String[1]        $archive_path  = '/usr/local/src/puppet-golang.tar.gz',
+  String[1]        $source_prefix = 'https://dl.google.com/go',
+  String[1]        $source        = "${source_prefix}/go${version}.${os}-${arch}.tar.gz",
+  String[1]        $os            = $facts['kernel'] ? {
     'Linux'  => 'linux',
     'Darwin' => 'darwin',
     default  => $facts['kernel']
   },
-  String[1] $arch = $facts['os']['hardware'] ? {
+  String[1]        $arch          = $facts['os']['hardware'] ? {
     undef    => 'amd64', # Darwin doesn't have os.hardware.
     'x86_64' => 'amd64',
     'i686'   => '386',
     default  => $facts['os']['hardware'],
   },
-  String[1] $source_prefix = 'https://dl.google.com/go',
-  String[1] $source = "${source_prefix}/go${version}.${os}-${arch}.tar.gz",
 ) {
   $archive_directory = regsubst($archive_path, '/[^/]+\Z', '')
   if ! defined(File[$archive_directory]) {
