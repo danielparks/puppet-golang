daaed4b5bd090c948f66caf9753ac6aa12524974
Daniel Parks <dp+git@oxidized.org>
2022-10-14 23:50:21 -0700

    pdk update: improvements to test.sh
    Added parameters for docker v. vagrant and for Puppet version.
    Unfortunately, using this with Vagrant on Ubuntu seems to be broken, but
    given how rairly I use the Vagrant option now I’m not going to worry
    about it.

--- test.sh
+++ test.sh
@@ -5,43 +5,63 @@ set -e
 export UTIL_TRACE=1
 source util.sh
 
+puppet=puppet7
+provider=docker
+
 usage () {
-  echo "Usage: $0 COMMAND [COMMAND...]"
+  echo "Usage: $0 [OPTION [OPTION...]] COMMAND [COMMAND...]"
+  echo ""
+  echo "OPTION may by one of:"
+  echo "  --docker      use docker for tests [default]"
+  echo "  --vagrant     use vagrant for tests"
+  echo "  --puppet6 -6  run tests in Puppet 6"
+  echo "  --puppet7 -7  run tests in Puppet 7 [default]"
   echo ""
   echo "COMMAND may be one of:"
-  echo "  init       initialize test set up"
-  echo "  snapshot   save a snapshot called 'general'"
-  echo "  restore    restore a snapshot called 'general'"
-  echo "  fast-init  restore to post-init state and reinstall the module"
-  echo "  update     reinstall module"
-  echo "  run        run tests"
-  echo "  destroy    destroy virtual machines or docker images"
-  echo "  docker-run run tests in docker (no need to init, this is all-in-one)"
+  echo "  init          initialize test set up"
+  echo "  update        reinstall module"
+  echo "  run           run tests"
+  echo "  destroy       destroy virtual machines or docker images"
+  echo ""
+  echo "If vagrant is used, the following additional commands are supported:"
+  echo "  snapshot      save a snapshot called 'general'"
+  echo "  restore       restore a snapshot called 'general'"
+  echo "  fast-init     restore to post-init state and reinstall the module"
+  echo ""
+  echo "Examples:"
+  echo "  $0 init run destroy"
+  echo "  $0 --vagrant -6 init run"
+  echo "  $0 --vagrant -6 fast-init run"
 }
 
 init () {
   # FIXME sometimes we just want to ensure that things are set up, and not
   # actually recreate everything
   destroy
-  rake 'litmus:provision_list[vagrant]'
 
-  bolt_task_run puppet_agent::install collection=puppet6 version=latest
-  bolt_task_run provision::fix_secure_path path=/opt/puppetlabs/bin
-  snapshot fresh
+  rake "litmus:provision_list[${provider}]"
+
+  if [[ $provider = vagrant ]] ; then
+    # There’s a bug in Litmus that prevents Puppet from being updated when a
+    # package is already available in the distro, e.g. on Ubuntu.
+    bolt_task_run puppet_agent::install "collection=${puppet}" version=latest
+    bolt_task_run provision::fix_secure_path path=/opt/puppetlabs/bin
+    snapshot fresh
+  else
+    rake "litmus:install_agent[${puppet}]"
+  fi
+
   rake litmus:install_module
-}
-
-docker-run () {
-  destroy
-  rake 'litmus:provision_list[docker]'
-  rake 'litmus:install_agent[puppet7]'
-  rake 'litmus:install_module'
-  rake 'litmus:acceptance:parallel'
-  rake 'litmus:tear_down'
 }
 
 snapshot () {
   local name=${1:-general}
+
+  if [[ $provider != 'vagrant' ]] ; then
+    echo 'snapshot command can only be used with vagrant.' >&2
+    exit 1
+  fi
+
   for box in .vagrant/*/Vagrantfile ; do
     (
       cd "$(dirname "$box")"
@@ -52,6 +72,12 @@ snapshot () {
 
 restore () {
   local name=${1:-general}
+
+  if [[ $provider != 'vagrant' ]] ; then
+    echo 'restore command can only be used with vagrant.' >&2
+    exit 1
+  fi
+
   for box in .vagrant/*/Vagrantfile ; do
     (
       cd "$(dirname "$box")"
@@ -61,6 +87,11 @@ restore () {
 }
 
 fast-init () {
+  if [[ $provider != 'vagrant' ]] ; then
+    echo 'fast-init command can only be used with vagrant.' >&2
+    exit 1
+  fi
+
   restore fresh
   rake litmus:install_module
 }
@@ -77,6 +108,7 @@ destroy () {
   if [ -f spec/fixtures/litmus_inventory.yaml ] ; then
     # If there’s no inventory, there’s nothing to tear down.
     rake litmus:tear_down
+    rm -f spec/fixtures/litmus_inventory.yaml
   fi
 }
 
@@ -87,8 +119,27 @@ fi
 
 for action in "$@" ; do
   case "$action" in
-    init|snapshot|restore|fast-init|update|run|destroy|docker-run) "$action" ;;
-    --help) usage ;;
-    *) usage >&2 ; exit 1 ;;
+    --vagrant)
+      provider=vagrant
+      ;;
+    --docker)
+      provider=docker
+      ;;
+    -6|--puppet6)
+      puppet=puppet6
+      ;;
+    -7|--puppet7)
+      puppet=puppet7
+      ;;
+    --help)
+      usage
+      ;;
+    init|snapshot|restore|fast-init|update|run|destroy)
+      "$action"
+      ;;
+    *)
+      usage >&2
+      exit 1
+      ;;
   esac
 done
